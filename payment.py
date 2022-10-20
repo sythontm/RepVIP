f579ac2111e114f4138693f4429908d58104b157
Hussein <115790078+vv9vv@users.noreply.github.com>
2022-10-20 16:14:56 +0300

    Update payment.py

--- payment.py
+++ payment.py
@@ -8,10 +8,10 @@ async def _(event):
     id = str(event.sender_id)
     idas = await sedthon.get_messages("sedupay", limit=1)
     msg = str(idas[0].message)
-    if id in msg and ispay[0] == 'no':
+    if id in msg and ispay[0] == 'yes':
         ispay.clear()
         ispay.append("yes")
-    elif id not in msg and ispay[0] == 'no':
+    elif id not in msg and ispay[0] == 'yes':
         ispay.clear()
         ispay.append("no")
     else:
@@ -20,11 +20,11 @@ async def _(event):
     id = str(event.sender_id)
     idas = await sedthon.get_messages("sedupay2", limit=1)
     msg = str(idas[0].message)
-    if id in msg and ispay2[0] == 'no':
+    if id in msg and ispay2[0] == 'yes':
         ispay2.clear()
         ispay2.append("yes")
-    elif id not in msg and ispay2[0] == 'no':
+    elif id not in msg and ispay2[0] == 'yes':
         ispay2.clear()
-        ispay2.append("no")
+        ispay2.append("yes")
     else:
         pass
